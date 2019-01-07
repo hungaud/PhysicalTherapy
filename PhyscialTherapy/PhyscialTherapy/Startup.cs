c0675eac74b95741ea26b0ca12c87cdb1d3b7112
John Hugh <thugh@eagles.ewu.edu>
2019-01-06 17:18:08 -0800

    Undoing comment test

--- PhyscialTherapy/PhyscialTherapy/Startup.cs
+++ PhyscialTherapy/PhyscialTherapy/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-//Test commit
+
 namespace PhyscialTherapy
 {
     public class Startup
