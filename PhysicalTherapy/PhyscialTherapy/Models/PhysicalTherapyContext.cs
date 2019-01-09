87de25eab8385ca69fe8ba5142c3e6c1e9ce460a
hungaud <hung.auduong@hotmail.com>
2019-01-09 12:57:59 -0800

    fix name

---
+++ PhysicalTherapy/PhyscialTherapy/Models/PhysicalTherapyContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhyscialTherapy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhysicalTherapy.Models
+{
+    public class PhysicalTherapyContext : DbContext
+    {
+
+        public DbSet<AccountType> AccountTypes { get; set; }
+        public DbSet<Administration> Administrations { get; set; }
+        public DbSet<Credential> Credentials { get; set; }
+        public DbSet<Excercise> Excercises { get; set; }
+        public DbSet<MessageLog> MessageLogs { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<PostRoutineSurvey> PostRoutineSurveys { get; set; }
+        public DbSet<Routine> Routines { get; set; }
+        public DbSet<RoutineExercise> RoutineExercises { get; set; }
+        public DbSet<Therapist> Therapists { get; set; }
+
+    }
+}
--- PhysicalTherapy/PhyscialTherapy/Startup.cs
+++ PhysicalTherapy/PhyscialTherapy/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
+using PhysicalTherapy.Models;
 
 namespace PhyscialTherapy
 {
@@ -28,9 +29,11 @@ namespace PhyscialTherapy
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().AddJsonOptions(option => option.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+            //services.AddScoped<I>
 
-            // connects to db. look in appsettings.json and change the local db name to what ever yours is. then exclude it from each commits.
-            //services.AddDbContext<PhysicalTherapy_Context>(options => options.UseSqlServer(Configuration.GetConnectionString("Development")));
+
+            //look in appsettings.json then exclude it from each commits.
+            services.AddDbContext<PhysicalTherapyContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Development")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
