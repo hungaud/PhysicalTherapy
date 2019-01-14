e2e0ff652dd4dc186858d59b10cf07654aca0087
John Hugh <thugh@eagles.ewu.edu>
2019-01-13 17:21:26 -0800

    Bare versions of the therapist home screen and roster
    Both currently fetch patients from a mock service instead of the real deal

--- PhysicalTherapy/PhyscialTherapy/Client/src/app/app-routing.module.ts
+++ PhysicalTherapy/PhyscialTherapy/Client/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TherapistHomeScreenComponent } from './therapist-home-screen/therapist-home-screen.component';
+import { TherapistRosterComponent } from './therapist-roster/therapist-roster.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'therapist-home-screen', component: TherapistHomeScreenComponent},
+  {path: 'therapist-roster-screen', component: TherapistRosterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- PhysicalTherapy/PhyscialTherapy/Client/src/app/app.module.ts
+++ PhysicalTherapy/PhyscialTherapy/Client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TherapistHomeScreenComponent } from './therapist-home-screen/therapist-home-screen.component';
+import { TherapistRosterComponent } from './therapist-roster/therapist-roster.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TherapistHomeScreenComponent,
+    TherapistRosterComponent
   ],
   imports: [
     BrowserModule,
