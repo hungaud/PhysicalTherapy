6f85312327d8324a8f8a9a70b74a87b9b4a5f92d
AlexLeathers <alexle27@gmail.com>
2019-01-21 18:51:26 -0800

    Add globals

--- PhysicalTherapy/PhyscialTherapy/Client/src/app/login/login.component.ts
+++ PhysicalTherapy/PhyscialTherapy/Client/src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
+import { apiEndpoint } from '../globals'
+
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
@@ -13,7 +15,7 @@ export class Login {
     // TODO: Add routing to patient homepage
     loginClick(username, password, error) {
         error.innerText = '';
-        this.http.get(`http://localhost:64496/api/Credentials/${username.value}`)
+        this.http.get(`${apiEndpoint}/Credentials/${username.value}`)
             .subscribe((response) => {
                 if (response['password'] !== password.value) {
                     error.innerText = 'INCORRECT USERNAME OR PASSWORD';
