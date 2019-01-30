df10b86b120e4196cb0e6640286a8e008afbaeb9
hungaud <hung.auduong@hotmail.com>
2019-01-30 11:40:10 -0800

    fix localhost for global.ts

--- PhysicalTherapy/PhyscialTherapy/Client/src/app/globals.ts
+++ PhysicalTherapy/PhyscialTherapy/Client/src/app/globals.ts
@@ -1,4 +1,5 @@
-export const apiEndpoint = 'http://localhost:64496/api';
+export const apiEndpoint = 'http://localhost:50000/api';
+//export const apiEndpoint = 'http://localhost:64496/api';
 //export const apiEndpoint = 'http://localhost:55818/api';
 //51671
 //export const apiEndpoint = 'http://localhost:51671/api';
