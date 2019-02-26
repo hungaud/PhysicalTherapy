9cd00891abb7c3caa48611b3dfaf3b8ef2359d1a
hungaud <hung.auduong@hotmail.com>
2019-02-25 20:20:39 -0800

    everything in test

--- PhysicalTherapy/PhyscialTherapy/Client/src/app/globals.ts
+++ PhysicalTherapy/PhyscialTherapy/Client/src/app/globals.ts
@@ -1,4 +1,6 @@
-export const apiEndpoint = 'http://localhost:50000/api';
+
+//export const apiEndpoint = 'http://localhost:50000/api';
+export const apiEndpoint = 'https://webapipt.azurewebsites.net/api';
 export const therapistId = 1;
 export const therapistId2 = 1001;
 
